a079977179afb1b14a01af59f24c26eebce54a33
Max Filipovich <fatumka@gmail.com>
2015-05-05 18:08:49 +0300

    add idle timeout, TTIN trap

--- lib/simple/sqs/worker.rb
+++ lib/simple/sqs/worker.rb
@@ -10,28 +10,12 @@ module Simple
     class Worker
       def initialize(options)
         @options = {poller_size: 1}.merge(options)
+        @pool = Thread.pool 1, @options[:worker_size]
       end
 
       def execute(&block)
-        pool = Thread.pool 1, @options[:worker_size]
-
-        subscribe_to_messages(pool, &block)
-
-        %w(SIGTERM INT).each do |signal|
-          trap signal do
-            p "Receive #{signal} signal. Shutting down pool..."
-
-            Thread.new do
-              @shutdown = true
-
-              pool.shutdown
-              (@poller_threads || []).map(&:exit)
-
-              p "All done. Exit..."
-              Process.exit(0)
-            end
-          end
-        end
+        subscribe_to_messages(&block)
+        trap_signals
 
         loop do
           sleep 1
@@ -40,37 +24,81 @@ module Simple
 
       private
 
-      def subscribe_to_messages(pool)
+      def subscribe_to_messages(&block)
+        queue = AWS::SQS.new.queues.named(@options[:queue])
+
         @poller_threads = @options[:poller_size].times.map do
           Thread.new do
             loop do
               break if @shutdown
 
-              queue.receive_message(limit: 10, wait_time_seconds: 20).
-                each do |message|
-                  break if @shutdown
-
-                  pool.process do
-                    begin
-                      unless @shutdown
-                        yield message
-                        message.delete
-                      end
-                    rescue StandardError => e
-                     p e.message
-                     raise e
-                   end
-                  end
-                end
-
-              sleep 0.1
+              handle(
+                queue.receive_message(limit: 10, wait_time_seconds: 20, idle_timeout: 20),
+                &block
+              )
             end
           end
         end
       end
 
-      def queue
-        @queue ||= AWS::SQS.new.queues.named(@options[:queue])
+      def handle(messages, &block)
+        messages.each do |message|
+          break if @shutdown
+
+          @pool.process do
+            begin
+              unless @shutdown
+                TuneMyGc.processing_started
+                block[message]
+                TuneMyGc.processing_ended
+
+                message.delete
+              end
+            rescue StandardError => e
+             p e.message
+             raise e
+           end
+          end
+        end
+      end
+
+      def trap_signals
+        trap 'TTIN' do
+          puts "   Thread count: #{Thread.list.count}"
+
+          puts "   Thread inspections:"
+          Thread.list.each do |thread|
+            puts "    #{thread.object_id}: #{thread.status}"
+          end
+
+          puts "   GC stats:"
+          puts GC.stat
+
+          puts "   Object Space:"
+
+          counts = Hash.new{ 0 }
+          ObjectSpace.each_object do |o|
+            counts[o.class] += 1
+          end
+
+          puts counts
+        end
+
+        %w(SIGTERM INT).each do |signal|
+          trap signal do
+            puts "Receive #{signal} signal. Shutting down pool..."
+
+            Thread.new do
+              @shutdown = true
+
+              @pool.shutdown
+              (@poller_threads || []).map(&:exit)
+
+              puts "All done. Exit..."
+              Process.exit(0)
+            end
+          end
+        end
       end
     end
   end
